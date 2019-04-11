953e7d2d1361e87523727afa27d3161fb536eead
ptlm500 <peter_mills@hotmail.co.uk>
2019-04-11 22:03:19 +0100

    Added basic movement handling

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./secrets.json');
+const Game = require('./lib/game/Game');
+
+const game = new Game(40, 20);
 
 client.once('ready', () => {
   console.log('Ready!');
@@ -9,5 +12,31 @@ client.once('ready', () => {
 client.login(config.token);
 
 client.on('message', message => {
-  console.log(message.content);
+  switch(message.content) {
+  case 'render':
+    message.reply(game.render(), {reply: false});
+    break;
+  case 'r':
+    game.tick(1, 0);
+    message.reply(game.render(), {reply: false});
+    break;
+  case 'l':
+    game.tick(-1, 1);
+    message.reply(game.render(), {reply: false});
+    break;
+  case 'u':
+    game.tick(0, -1);
+    message.reply(game.render(), {reply: false});
+    break;
+  case 'd':
+    game.tick(0, 1);
+    message.reply(game.render(), {reply: false});
+    break;
+  default:
+    break;
+  }
+
+  // console.log(message.content);
+  // console.log(game.render());
+  // message.reply();
 });
