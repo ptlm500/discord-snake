676e7ec0f612d5b04f1d2f4dbfd89f4a63e60c13
ptlm500 <peter_mills@hotmail.co.uk>
2019-05-05 23:46:36 +0100

    Refactor into GameController and VotingHandler

--- index.js
+++ index.js
@@ -3,23 +3,17 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
-const Game = require('./lib/game/Game');
-const VotingHandler = require('./lib/voting-handler/VotingHandler');
+const GameController = require('./lib/game-controller');
 const logger = require('./lib/logger');
-
 
 const info = {
   restarts: 0
 };
 
-let game;
-let currentGameMessage;
-let votingHandler;
-let collector;
-let onTick;
+let gameController;
 
 client.once('ready', () => {
-  game = new Game(config.gameHeight, config.gameWidth);
+  gameController = new GameController(client);
   logger.info({message: 'Client ready!'});
 });
 
@@ -33,53 +27,22 @@ client.on('error', error => {
   }
 });
 
-function collectorFilter(reaction, user) {
-  if (user.id === client.user.id) {
-    return false;
-  } else if (!isAControlEmoji(reaction.emoji.name, controlEmojis)) {
-    return false;
-  }
-  return true;
-}
-
-function isAControlEmoji(emoji, controlEmojis) {
-  return Object.keys(controlEmojis).includes(emoji);
-}
-
-const controlEmojis = {
-  '◀': 'left',
-  '▶': 'right',
-  '🔼': 'up',
-  '🔽': 'down'
-};
-
 client.on('message', message => {
   handleAdminMessage(message);
 
   if (message.channel.id === process.env.ALLOWED_CHANNEL) {
     if (message.author.id === client.user.id) {
-      currentGameMessage = message;
-      if (collector || game.gameOver) {
-        collector.stop();
-      }
-
-      if (!game.gameOver) {
-        reactToWithEmojis(message, Object.keys(controlEmojis));
-        addReactionCollector(message);
-      }
+      gameController.handleNewGameMessage(message);
     }
 
     switch(message.content) {
     case `${config.prefix}render`:
       logger.info('Rendered by %s', message.author.username);
-      game.start();
-      message.reply(game.message, {reply: false});
+      message.reply(gameController.start(), {reply: false});
       break;
     case `${config.prefix}restart`:
       logger.info('Game restarted by %s', message.author.username);
-      game = new Game(config.gameHeight, config.gameWidth);
-      game.start();
-      message.reply(game.message, {reply: false});
+      message.reply(gameController.restart(), {reply: false});
       break;
     default:
       break;
@@ -87,46 +50,10 @@ client.on('message', message => {
   }
 });
 
-async function reactToWithEmojis(message, emojis) {
-  for (const emoji of emojis) {
-    try {
-      await message.react(emoji);
-    } catch(error) {
-      logger.error('Failed to react with emoji', error);
-    }
-  }
-}
-
-async function advanceGame(message, nextMove) {
-  logger.info({message: 'Advancing with move', nextMove});
-  game.tick(nextMove);
-  await message.reply(game.message, {reply: false});
-  if (!game.gameOver && config.deletePreviousMessages) {
-    await message.delete();
-  }
-}
-
 async function restartClient() {
   info.restarts = info.restarts + 1;
 
-  if (collector) {
-    try {
-      collector.stop();
-      logger.debug('Stopped collector');
-    } catch (error) {
-      logger.error('Failed to stop collector', error);
-    }
-  }
-
-  if (onTick) {
-    try {
-      clearTimeout(onTick);
-      logger.debug('Stopped old onTick');
-    } catch (error) {
-      logger.error('Failed to stop old onTick', error);
-    }
-  }
-
+  gameController.stopVoteCollection();
   logger.info('Restarting client');
   try {
     await client.destroy();
@@ -144,9 +71,7 @@ async function restartClient() {
     return;
   }
 
-  if (currentGameMessage && votingHandler && votingHandler.getChosenVote()) {
-    advanceGame(currentGameMessage, votingHandler.getChosenVote());
-  }
+  gameController.resume();
 }
 
 function getStatus() {
@@ -154,30 +79,6 @@ function getStatus() {
 uptime:   ${client.uptime}
 restarts: ${info.restarts}
   `;
-}
-
-function addReactionCollector(message) {
-  let votingTimerStarted = false;
-  votingHandler = new VotingHandler(controlEmojis);
-
-  collector = message.createReactionCollector(collectorFilter);
-  collector.on('collect', (reaction) => {
-    if (!votingTimerStarted) {
-      onTick = setTimeout(
-        () => advanceGame(message, votingHandler.getChosenVote()),
-        config.voteTimerInSecs * 1000
-      );
-
-      votingTimerStarted = true;
-      logger.debug({message: 'Started voting timer'});
-    }
-    logger.debug({
-      message: 'Adding vote',
-      name: reaction.emoji.name,
-      move: controlEmojis[reaction.emoji.name]
-    });
-    votingHandler.updateVotes(controlEmojis[reaction.emoji.name], reaction.count - 1);
-  });
 }
 
 function handleAdminMessage(message) {
